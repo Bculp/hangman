b6f7cf415bcbcaa5ad7932f7e3728b6452327fe1
Brandon Culp <bculp513@gmail.com>
2018-03-08 22:02:04 -0500

    Code clean up and linter fixes

--- browser/app/components/Game.js
+++ browser/app/components/Game.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Button, Modal, Visibility, Input } from 'semantic-ui-react';
 
-const initialState = {categories: [], category: '', lives: 5, score: 0, word: '', guess: [], status: '', pastWords: [], mode: ''};
+const initialState = {categories: [], category: '', lives: 5, word: '', guess: [], status: '', mode: ''};
 const alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 const staticCategories = ['Activity 1', 'Activity 2', 'Activity 3'];
 const answers = ['art', 'fruity deliciousness', 'seasonal dinner'];
-let disabledBtns = [], activeCategory = {};
+let disabledBtns = [];
 
 export default class Game extends Component {
   constructor(props) {
@@ -21,8 +21,8 @@ export default class Game extends Component {
   componentDidMount() {
     this.setState({
       categories: staticCategories
-    })
-    this.refs.start_game.handleOpen()
+    });
+    this.refs.start_game.handleOpen();
   }
 
   startGame() {
@@ -37,11 +37,11 @@ export default class Game extends Component {
     let answer;
     let guess;
     function createPlaceholder() {
-      let spaceIndex = answer.indexOf(' ');
+      const spaceIndex = answer.indexOf(' ');
       guess = '_'.repeat(answer.length).split('');
       guess[spaceIndex] = ' ';
     }
-    
+
     if (text === 'Activity 1') {
       answer = answers[0];
       createPlaceholder();
@@ -56,7 +56,8 @@ export default class Game extends Component {
       lives: 5,
       word: answer,
       category: text,
-      guess
+      guess,
+      status: ''
     });
     this.enableBtns();
     this.refs.start_game.handleClose();
@@ -103,7 +104,7 @@ export default class Game extends Component {
     })
   }
 
-  playAgain(yes) {
+  playAgain() {
     this.refs.play_again.handleClose();
     this.refs.start_game.handleOpen();
     this.enableBtns();
@@ -112,8 +113,8 @@ export default class Game extends Component {
 
   enableBtns() {
     disabledBtns.forEach(btn => {
-      btn.disabled = false
-    })
+      btn.disabled = false;
+    });
     disabledBtns = [];
   }
 
@@ -125,25 +126,45 @@ export default class Game extends Component {
             <h3 className="center">Welcome to Hangman! </h3>
             <h5 className="center">Select an activity</h5>
             <div className="center">
-              <Button className="center" onClick={(e) => this.handleClick(e.target)}>Activity 1</Button>
-              <Button className="center" onClick={(e) => this.handleClick(e.target)}>Activity 2</Button>
-              <Button className="center" onClick={(e) => this.handleClick(e.target)}>Activity 3</Button>
+              <Button
+                className="center"
+                onClick={e => this.handleClick(e.target)}
+              >Activity 1
+              </Button>
+              <Button
+                className="center"
+                onClick={e => this.handleClick(e.target)}
+              >Activity 2
+              </Button>
+              <Button
+                className="center"
+                onClick={e => this.handleClick(e.target)}
+              >Activity 3
+              </Button>
             </div>
           </Modal.Content>
         </Modal>
         <Modal ref="play_again" size="tiny">
           <Modal.Content>
-          <h3 className="center">{`${this.state.status} The answer is: ${this.state.word}`}</h3>
-          <h5 className="center">Play Again?</h5>
-          <div className="center">
-            <Button onClick={this.playAgain}>Yes</Button>
-            <Button onClick={() => this.refs.play_again.handleClose()}>No</Button>
-          </div>
+            <h3 className="center">{`${this.state.status} The answer is: ${this.state.word}`}</h3>
+            <h5 className="center">Play Again?</h5>
+            <div className="center">
+              <Button onClick={this.playAgain}>Yes</Button>
+              <Button onClick={() => this.refs.play_again.handleClose()}>No</Button>
+            </div>
           </Modal.Content>
         </Modal>
         <ul className="center categories">
           {staticCategories.map((category, index) => {
-            return <Button color="olive" key={index} compact onClick={(e) => this.handleClick(e.target)}>{category}</Button>
+            return (
+              <Button
+                color="olive"
+                key={index}
+                compact
+                onClick={(e) => this.handleClick(e.target)}
+              >{category}
+              </Button>
+            );
           })
         }
         </ul>
@@ -156,7 +177,14 @@ export default class Game extends Component {
         <div className="center guesses">
           {
             alpha.map((letter, index) => {
-              return <Button circular key={index} onClick={(e) => this.handleGuess(e.target)}>{letter}</Button>
+              return (
+                <Button
+                  circular
+                  key={index}
+                  onClick={(e) => this.handleGuess(e.target)}
+                >{letter}
+                </Button>
+              );
             })
           }
         </div>
@@ -165,6 +193,6 @@ export default class Game extends Component {
           {this.state.status === 'You Won!' || this.state.status === 'You Lost!' ? this.refs.play_again.handleOpen() : ''}
         </div>
       </div>
-    )
+    );
   }
 }
